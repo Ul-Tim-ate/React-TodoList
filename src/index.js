799b57d273a6a1327eec13628533efe730c7061e
Timofey Trakhanov <timohator1@mail.ru>
2022-06-09 18:22:06 +0300

    create first react element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log(213);
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = (
+	<div>
+		<h1>My ToDo List</h1>
+		<input placeholder="search" />
+		<ul>
+			<li>Learn React</li>
+			<li>Build Awesome App</li>
+		</ul>
+	</div> );
+ReactDOM.render(el, document.getElementById('root'));
