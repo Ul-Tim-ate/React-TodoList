183e75c4f3b392db87e84f0cf9a83ad87b3ede4c
Timofey Trakhanov <timohator1@mail.ru>
2022-06-10 18:40:15 +0300

    create folder for every component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-import ItemStatusFilter from "./components/item-status-filter";
+import AppHeader from "./components/app-header/app-header";
+import SearchPanel from "./components/search-panel/search-panel";
+import TodoList from "./components/todo-list/todo-list";
+import ItemStatusFilter from "./components/item-status-filter/item-status-filter";
 
 import "./index.css";
 
