3432a5c9e3d54f8530d84a0f2917013ef66e5d2d
Timofey Trakhanov <timohator1@mail.ru>
2022-06-10 10:53:41 +0300

    put components in separate files in folder components

---
+++ src/components/todo-list.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { TodoListItem } from "./todo-list-item";
+
+export const TodoList = () => {
+  return (
+    <ul>
+      <li>
+        <TodoListItem />
+      </li>
+      <li>
+        <TodoListItem />
+      </li>
+    </ul>
+  );
+};
