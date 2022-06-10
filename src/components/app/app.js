a427081e56d33ed0f2743b2d0f72356bd72d277e
Timofey Trakhanov <timohator1@mail.ru>
2022-06-10 19:15:57 +0300

    create folder for app component

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+import TodoList from "../todo-list";
+import ItemStatusFilter from "../item-status-filter";
+
+import "./app.css";
+
+export const App = () => {
+  const todoData = [
+    { label: "Drink Coffee", important: false, id: 1 },
+    { label: "Make Awesome App", important: true, id: 2 },
+    { label: "Have a lunch", important: false, id: 3 },
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ src/components/todo-list-item/index.js
@@ -0,0 +1,2 @@
+import TodoListItem  from "./item-status-filter";
+export default TodoListItem;
